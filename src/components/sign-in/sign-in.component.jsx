b42975fdf49fa286cc7b613798997ce6ea855b86
Robert <Zobrist@Roberts-MacBook-Pro.local>
2021-10-28 23:01:22 -0700

    added facebook oauth flow, along with sign in btn. feature fully functional

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithFacebook, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -62,7 +62,10 @@ class SignIn extends React.Component {
           <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign in with Google
+               Google
+            </CustomButton>
+            <CustomButton onClick={signInWithFacebook} isFacebookSignIn>
+               Facebook
             </CustomButton>
           </div>
         </form>
