48e85fcee7f821ebffbf4ee65e14de2ff2dc2131
Robert <Zobrist@Roberts-MacBook-Pro.local>
2021-10-31 16:49:33 -0700

    migrated shop data from shop component to redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,7 +7,7 @@ import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
-let renderCount = 1;
+
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
