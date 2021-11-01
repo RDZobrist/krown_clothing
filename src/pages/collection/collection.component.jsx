764b23ddb3a32212e7c448d71b936ae96790f891
Robert <Zobrist@Roberts-MacBook-Pro.local>
2021-11-01 07:57:47 -0700

    added dynamic routing for the shop page, what collection to render

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
-
+import { selectCollection, selectCollections } from "../../redux/shop/shop.selectors";
 import './collection.styles.scss';
 
-const CollectionPage = () => (
+const CollectionPage = ({ collection }) => (
     <div className="collection-page">
         <h2>CATEGORY PAGE coming soon...</h2>
     </div>
 )
-
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+export default connect(mapStateToProps)(CollectionPage)
