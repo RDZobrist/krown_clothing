01e546222e97a0c466b27f258ec35e034b8adea5
Robert <Zobrist@Roberts-MacBook-Pro.local>
2021-11-01 07:03:48 -0700

    added collections-overview component and scss file

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import './collection.styles.scss';
+
+const CollectionPage = () => (
+    <div className="collection-page">
+        <h2>CATEGORY PAGE coming soon...</h2>
+    </div>
+)
+
+export default CollectionPage
