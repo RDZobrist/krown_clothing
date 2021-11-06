04670ff7024eb77ac4eea0f5621543690ac56d24
Robert <Zobrist@Roberts-MacBook-Pro.local>
2021-11-05 21:40:42 -0700

    editied the design of our isLoading widget

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
-
+import './spinner.styles.scss'
 const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
     return isLoading ? (
         <SpinnerOverlay>
-            <SpinnerContainer /><br />
-            <SpinnerContainer /><br />
-            <br />
-            <h2><br />  We're working on it...  </h2>
-            <SpinnerContainer /><br />
-            <SpinnerContainer /><br />
-        </SpinnerOverlay>
+                            <h1 className='spinner-title'>Almost there. . .</h1>
 
+            <div class="sk-cube-grid">
+                <div class="sk-cube sk-cube1"></div>
+                <div class="sk-cube sk-cube2"></div>
+                <div class="sk-cube sk-cube3"></div>
+                <div class="sk-cube sk-cube2"></div>
+                <div class="sk-cube sk-cube7"></div>
+                <div class="sk-cube sk-cube7"></div>
+                <div class="sk-cube sk-cube1"></div>
+                <div class="sk-cube sk-cube3"></div>
+                <div class="sk-cube sk-cube5"></div>
+                <div class="sk-cube sk-cube7"></div>
+                <div class="sk-cube sk-cube5"></div>
+                <div class="sk-cube sk-cube3"></div>
+                <div class="sk-cube sk-cube1"></div>
+                <div class="sk-cube sk-cube3"></div>
+                <div class="sk-cube sk-cube5"></div>
+                <div class="sk-cube sk-cube7"></div>
+                <div class="sk-cube sk-cube6"></div>
+                <div class="sk-cube sk-cube4"></div>
+                <div class="sk-cube sk-cube2"></div>
+                <div class="sk-cube sk-cube1"></div>
+                <div class="sk-cube sk-cube4"></div>
+                <div class="sk-cube sk-cube6"></div>
+                <div class="sk-cube sk-cube4"></div>
+                <div class="sk-cube sk-cube2"></div>
+                <div class="sk-cube sk-cube4"></div>
+                <div class="sk-cube sk-cube6"></div>
+                <div class="sk-cube sk-cube4"></div>
+                <div class="sk-cube sk-cube6"></div>
+    
+
+            </div>
+        </SpinnerOverlay>
     ) : (
         <WrappedComponent {...otherProps} />
     )
--- src/components/with-spinner/with-spinner.styles.jsx
+++ src/components/with-spinner/with-spinner.styles.jsx
@@ -1,25 +1,22 @@
 import styled from 'styled-components';
 
 export const SpinnerOverlay = styled.div`
-  height: 65vh;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+min-width: 25vw;
+height: 4.237vh;
 `;
 
 export const SpinnerContainer = styled.div`
   display: inline-block;
   width: 90px;
   height: 90px;
-  border: 1.234567rem solid rgba(138, 138, 138, 0.48);
-  border-radius: 50%;
+  border: 2.12rem solid rgba(113, 113, 113, 0.48);
+  border-radius: 520%;
   border-top-color: #626767;
-  animation: spin 900ms ease-in-out infinite;
-  -webkit-animation: spin 950ms ease-in-out infinite;
+  animation: spin 585ms ease-in-out infinite;
+  -webkit-animation: spin 585ms ease-in-out infinite;
   @keyframes spin {
     to {
-      -webkit-transform: rotate(900deg);
+      -webkit-transform: rotate(540deg);
     }
   }
   @-webkit-keyframes spin {
